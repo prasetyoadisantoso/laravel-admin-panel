f4ac0831a16ffa07686327849322e291fedfda2e
Prasetyo Adi Santoso <prasetyoadisantoso@gmail.com>
2021-04-13 15:20:42 +0800

    Minor fixing for edit setting

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -65,6 +65,13 @@ class SettingController extends Controller
         return $settings;
     }
 
+    public function edit($id)
+    {
+        $settings = Setting::find($id);
+
+        return $settings;
+    }
+
     /**
      * Update the specified resource in storage.
      *
